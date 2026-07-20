f2f63ab67f10d631e9f52e77ea6b36cf23dcb505
kggarg78901 <kartikgarg146@gmail.com>
2026-07-20 23:30:02 +0530

    Updated ui for forgot password

--- frontend/src/pages/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import react, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FiArrowLeft, FiLock } from "react-icons/fi";
 import "./ForgotPassword.css";
--- frontend/src/pages/VerifyOtp/VerifyOtp.jsx
+++ frontend/src/pages/VerifyOtp/VerifyOtp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import react, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import "./VerifyOtp.css";
