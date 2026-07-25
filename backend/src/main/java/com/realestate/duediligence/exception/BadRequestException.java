944c1ce190ba35ebe780e2e8e30cc5599c7502aa
immu-06 <forfinance3113@gmail.com>
2026-07-25 09:13:40 +0530

    Create BadRequestException.java

---
+++ backend/src/main/java/com/realestate/duediligence/exception/BadRequestException.java
@@ -0,0 +1,10 @@
+package com.realestate.duediligence.exception;
+
+import org.springframework.http.HttpStatus;
+
+
+public class BadRequestException extends ApiException {
+    public BadRequestException(String message) {
+        super(message, HttpStatus.BAD_REQUEST);
+    }
+}
